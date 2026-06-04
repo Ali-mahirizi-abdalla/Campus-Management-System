7eab01b9b0518f9bbe30b7185d245ad148c3c4bb
Ali-mahirizi-abdalla <alimahrez744@gmail.com>
2026-06-04 23:48:07 +0300

    fix: use --fake-initial to handle partial library migrations on production db

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ pip install cryptography
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py migrate --fake-initial --noinput
